14a3e181914d5eed7c0082b331bab27014e78794
Janus Roestenburg <JanusRoestenburg@gmail.com>
2024-10-28 10:38:14 +0100

    Fixed attribute level prompting with accuracy measures

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,9 @@ import os
 import pandas as pd
 
 from services.attribute_prompt import attribute_prompt
+from utils.accuracy_utils import calculate_metrics
+from utils.data_utils import annotate_errors
+from utils.output_utils import process_attribute_output
 
 # # from modules import tuple_analyzer
 # from backend.modules.AttributeProcessor import attribute_prompt
@@ -22,11 +25,11 @@ from config import DevelopmentConfig
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 app.secret_key = "supersecretkey"
-app.config["UPLOAD_FOLDER"] = "uploads"
+# app.config["UPLOAD_FOLDER"] = "uploads"
 app.config["ALLOWED_EXTENSIONS"] = {"csv"}
 
 # Ensure the upload folder exists
-os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+# os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 
 def allowed_file(filename):
@@ -148,11 +151,41 @@ def detect_attribute_errors():
     try:
         dataset = pd.read_csv("./data/dataset.csv")
         attribute_prompt(dataset, "./data/output/attribute")
+        process_attribute_output(dataset, "./data/output/attribute")
 
         return (
             jsonify(
                 {
                     "message": "Attribute errors detected!",
+                }
+            ),
+            200,
+        )
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/calculate-accuracy", methods=["GET"])
+def calculate_accuracy():
+    try:
+        dataset = pd.read_csv("./data/dataset.csv")
+        clean_dataset = pd.read_csv("./data/cleaned_dataset.csv")
+
+        error_annotation = annotate_errors(clean_dataset, dataset)
+        output = pd.read_csv("./data/output/attribute/output.csv")
+
+        accuracy, precision, recall, f_score = calculate_metrics(
+            error_annotation, output
+        )
+
+        return (
+            jsonify(
+                {
+                    "accuracy": accuracy,
+                    "precision": precision,
+                    "recall": recall,
+                    "f_score": f_score,
                 }
             ),
             200,

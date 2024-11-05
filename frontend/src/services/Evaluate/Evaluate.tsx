072b1cf15ec7e3d99189bb6c07912e0f6227763c
Janus Roestenburg <JanusRoestenburg@gmail.com>
2024-11-05 09:08:23 +0100

    Added new requests to frontend

---
+++ frontend/src/services/Evaluate/Evaluate.tsx
@@ -0,0 +1,76 @@
+import { application_service_url } from "@/config/config";
+
+export async function evaluateAttributeErrors(
+  datasetName: string,
+  timestamp: string
+) {
+  try {
+    const url = `${application_service_url}/evaluate-attribute-errors?dataset_name=${encodeURIComponent(
+      datasetName
+    )}&timestamp=${encodeURIComponent(timestamp)}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to detect: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error during error detection:", error);
+    throw error;
+  }
+}
+
+export async function evaluateDepViolations(
+  datasetName: string,
+  timestamp: string
+) {
+  try {
+    const url = `${application_service_url}/evaluate-dependency-violation-errors?dataset_name=${encodeURIComponent(
+      datasetName
+    )}&timestamp=${encodeURIComponent(timestamp)}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to detect: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error during dependency violation detection:", error);
+    throw error;
+  }
+}
+
+export async function evaluateCombinedResults(
+  datasetName: string,
+  timestamp: string
+) {
+  try {
+    const url = `${application_service_url}/evaluate-combined-errors?dataset_name=${encodeURIComponent(
+      datasetName
+    )}&timestamp=${encodeURIComponent(timestamp)}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to detect: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error during combined result retrieval:", error);
+    throw error;
+  }
+}
--- frontend/src/services/Utils/Utils.tsx
+++ frontend/src/services/Utils/Utils.tsx
@@ -5,6 +5,12 @@ type UploadParams = {
   datasetName: string;
 };
 
+type UploadEvaluateParams = {
+  clean_file: File;
+  dirty_file: File;
+  datasetName: string;
+};
+
 export async function uploadDataset({ file, datasetName }: UploadParams) {
   const formData = new FormData();
   formData.append("file", file);
@@ -15,6 +21,37 @@ export async function uploadDataset({ file, datasetName }: UploadParams) {
       method: "POST",
       body: formData,
     });
+
+    if (!response.ok) {
+      throw new Error(`Failed to upload: ${response.statusText}`);
+    }
+
+    // Return the parsed JSON data directly if the response is successful
+    return await response.json();
+  } catch (error) {
+    console.error("Error during file upload:", error);
+    throw error; // Re-throw to be caught in handleFileChange
+  }
+}
+
+export async function uploadEvaluateDataset({
+  clean_file,
+  dirty_file,
+  datasetName,
+}: UploadEvaluateParams) {
+  const formData = new FormData();
+  formData.append("clean_file", clean_file);
+  formData.append("dirty_file", dirty_file);
+  formData.append("dataset_name", datasetName);
+
+  try {
+    const response = await fetch(
+      `${application_service_url}/upload-evaluation-datasets`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Failed to upload: ${response.statusText}`);

48fbc2f79934e798d55eb2430f02dc0e38256095
Janus Roestenburg <JanusRoestenburg@gmail.com>
2024-12-03 15:56:46 +0100

    Dockerized forntend

--- frontend/src/config/config.tsx
+++ frontend/src/config/config.tsx
@@ -1,3 +1,4 @@
-const application_service_url = "http://localhost:5000";
+// const application_service_url = "http://localhost:5000";
+const application_service_url = "http://dataset_analyzer:5000";
 
 export { application_service_url };
